439d32176634b06ac68f12747e5e17ab1357784f
Denys Bzenko <dbzenko@kse.org.ua>
2024-06-30 20:21:52 +0300

    NOT

--- class/class5.1/MegaData.cpp
+++ class/class5.1/MegaData.cpp
@@ -1,11 +1,1 @@
-#include "MegaData.hpp"
-
-MegaData::MegaData() {
-    smallArray.fill(42.0f);
-    bigArray.fill(42.0);
-}
-
-void MegaData::reset() {
-    smallArray.fill(42.0f);
-    bigArray.fill(42.0);
-}
+!
--- class/class5.1/MegaData.hpp
+++ class/class5.1/MegaData.hpp
@@ -1,15 +1,1 @@
-#ifndef MEGADATA_HPP
-#define MEGADATA_HPP
-
-#include <array>
-
-class MegaData {
-public:
-    std::array<float, 1024> smallArray;
-    std::array<double, 1024 * 1024> bigArray;
-
-    MegaData();
-    void reset();
-};
-
-#endif // MEGADATA_HPP
+!
